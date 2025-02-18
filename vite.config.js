6dfbd3395fd77068968480c3b6b0c7f65fcd8e5f
TatianaSerg <aliece87@gmail.com>
2025-02-19 01:51:30 +0200

    Updated routes in vite.config.js  for base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/a-to-do-list-app/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
